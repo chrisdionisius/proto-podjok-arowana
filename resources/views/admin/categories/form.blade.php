a8ea945160591c84ae53ac02c0de828a3a815f07
chrisdionisius <chrisdionisius@gmail.com>
2021-04-18 23:10:04 +0700

    add category

--- resources/views/admin/categories/form.blade.php
+++ resources/views/admin/categories/form.blade.php
@@ -1,52 +1,43 @@
 @extends('admin.layout')
+
 @section('content')
-    <div class="content">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card card-default">
-                    <div class="card-header card-header-border-bottom">
-                        <h2>Categories</h2>
-                    </div>
-                    <div class="card-body">
-                        @include('admin.partials.flash')
-                        <table class="table table-bordered table-stripped">
-                            <thead>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th>Slug</th>
-                                <th>Parent</th>
-                                <th>Action</th>
-                            </thead>
-                            <tbody>
-                                @forelse ($categories as $category)
-                                    <tr>    
-                                        <td>{{ $category->id }}</td>
-                                        <td>{{ $category->name }}</td>
-                                        <td>{{ $category->slug }}</td>
-                                        <td>{{ $category->parent_id }}</td>
-                                        <td>
-                                            <a href="{{ url('admin/categories/'. $category->id .'/edit') }}" class="btn btn-warning btn-sm">edit</a>
-                                            
-                                            {!! Form::open(['url' => 'admin/categories/'. $category->id, 'class' => 'delete', 'style' => 'display:inline-block']) !!}
-                                            {!! Form::hidden('_method', 'DELETE') !!}
-                                            {!! Form::submit('remove', ['class' => 'btn btn-danger btn-sm']) !!}
-                                            {!! Form::close() !!}
-                                        </td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="5">No records found</td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                        {{ $categories->links() }}
-                    </div>
-                    <div class="card-footer text-right">
-                        <a href="{{ url('admin/categories/create') }}" class="btn btn-primary">Add New</a>
-                    </div>
+    
+@php
+    $formTitle = !empty($category) ? 'Update' : 'New'    
+@endphp
+
+<div class="content">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card card-default">
+                <div class="card-header card-header-border-bottom">
+                        <h2>{{ $formTitle }} Category</h2>
                 </div>
-            </div>
+                <div class="card-body">
+                    @include('admin.partials.flash', ['$errors' => $errors])
+                    @if (!empty($category))
+                        {!! Form::model($category, ['url' => ['admin/categories', $category->id], 'method' => 'PUT']) !!}
+                        {!! Form::hidden('id') !!}
+                    @else
+                        {!! Form::open(['url' => 'admin/categories']) !!}
+                    @endif
+                        <div class="form-group">
+                            {!! Form::label('name', 'Name') !!}
+                            {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'category name']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('parent_id', 'Parent') !!}
+                            
+                            {!! General::selectMultiLevel('parent_id', $categories, ['class' => 'form-control', 'selected'=>(!empty(old('parent_id')) ? old('parent_id') : !empty($category['parent_id'])) ? $category['parent_id'] : '', 'placeholder'=>'---Chose Category---' ]) !!}
+                        </div>
+                        <div class="form-footer pt-5 border-top">
+                            <button type="submit" class="btn btn-primary btn-default">Save</button>
+                            <a href="{{ url('admin/categories') }}" class="btn btn-secondary btn-default">Back</a>
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>  
         </div>
     </div>
+</div>
 @endsection
